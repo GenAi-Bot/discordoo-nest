4ec006f7889c36b611a6835983a5f9395347229e
bit0r1n <me@bitor.in>
2025-04-28 17:48:33 +0300

    module: isGlobal value

--- src/discordoo.module-definition.ts
+++ src/discordoo.module-definition.ts
@@ -2,4 +2,14 @@ import { DiscordooOptions } from './interfaces'
 import { ConfigurableModuleBuilder } from '@nestjs/common'
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<DiscordooOptions>().build()
+  new ConfigurableModuleBuilder<DiscordooOptions>()
+    .setExtras(
+      {
+        isGlobal: false,
+      },
+      (definition, extras) => ({
+        ...definition,
+        global: extras.isGlobal,
+      }),
+    )
+    .build()
